e89ffc2cacf33becdc0a5f187467b0d10fe4a3cc
jpcq00x <jpcq_00x@sina.com>
2015-05-06 21:09:27 +0800

    第4次总体提交。。。。。。。

--- HelloWeb01/src/main/java/cn/msdd/pcq/App.java
+++ HelloWeb01/src/main/java/cn/msdd/pcq/App.java
@@ -4,13 +4,7 @@ package cn.msdd.pcq;
  * Created by Administrator on 2015/5/6.
  */
 public class App {
-    public static void main(String str[]) {
-        System.out.print("99999999999999");
-        System.out.print("99999999999999");
-        System.out.print("99999999999999");
-        System.out.print("99999999999999");
-        System.out.print("99999999999999");
-        System.out.print("99999999999999");
-        System.out.print("99999999999999");
+    public void method() {
+        System.out.print("");
     }
 }
